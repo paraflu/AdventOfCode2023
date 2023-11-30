0974af29693a58c074ba22c94a5c38b5d2799ee9
Andrea Forlin <andrea.forlin@gmail.com>
2023-11-30 21:57:22 +0100

    new: dev: day2

--- 2022/Day2/Program.fs
+++ 2022/Day2/Program.fs
@@ -24,6 +24,16 @@ let castMe me =
     | "Y" -> Me.Paper
     | "Z" -> Me.Scissor
     | _ -> failwith (sprintf "Valore errato '%s'" me)
+
+type Result = Loose | Draw | Win
+
+let castResult res = 
+    match res with 
+    | "X" -> Result.Loose
+    | "Y" -> Result.Draw
+    | "Z" -> Result.Win
+    | _ -> failwith (sprintf "Valore errato '%s'" res)
+
 let day2_part1 filename =
 
     let response = readAllLinesAsync filename
@@ -32,15 +42,15 @@ let day2_part1 filename =
                                         |> Seq.map (fun a ->
                                             let x = (castHim a.[0], castMe a.[1])
                                             match x with
-                                            | ( Him.Rock, Me.Rock ) -> (1 + 1)
-                                            | ( Him.Paper, Me.Rock ) -> (1 + 2) 
-                                            | ( Him.Scissor, Me.Rock ) -> (1 + 3) * 2
-                                            | ( Him.Rock, Me.Paper ) -> (2 + 1) * 2
-                                            | ( Him.Paper, Me.Paper ) -> (2 + 2) 
-                                            | ( Him.Scissor, Me.Paper ) -> (2 + 3) 
-                                            | ( Him.Rock, Me.Scissor ) -> (3 + 1)  
-                                            | ( Him.Paper, Me.Scissor ) -> (3 + 2) * 2
-                                            | ( Him.Scissor, Me.Scissor ) -> (3 + 3)
+                                            | ( Him.Rock, Me.Rock ) -> 1 + 3
+                                            | ( Him.Paper, Me.Rock ) -> 1 + 0
+                                            | ( Him.Scissor, Me.Rock )  -> 1 + 6
+                                            | ( Him.Rock, Me.Paper ) -> 2 + 6
+                                            | ( Him.Paper, Me.Paper ) -> 2 + 3
+                                            | ( Him.Scissor, Me.Paper ) -> 2 + 0
+                                            | ( Him.Rock, Me.Scissor ) -> 3 + 0
+                                            | ( Him.Paper, Me.Scissor ) -> 3 + 6
+                                            | ( Him.Scissor, Me.Scissor ) -> 3 + 3
                                             | _ -> -1
                                         )
                                         |> Seq.reduce (+)
@@ -49,9 +59,28 @@ let day2_part1 filename =
     printf "part1: %d\n" response
 
 let day2_part2 filename =
-    let rows = readAllTextAsync filename |> Async.RunSynchronously
 
-    printf "part2: %d\n" 0
+    let response = readAllLinesAsync filename
+                    |> Async.RunSynchronously
+                    |> Seq.map (fun s -> s.Split(' '))
+                    |> Seq.map (fun a ->
+                        let x = (castHim a.[0], castResult a.[1])
+                        match x with
+                        | ( Him.Rock, Result.Draw ) -> 1 + 3
+                        | ( Him.Paper, Result.Loose ) -> 1 + 0
+                        | ( Him.Scissor, Result.Win )  -> 1 + 6
+                        | ( Him.Rock, Result.Win ) -> 2 + 6
+                        | ( Him.Paper, Result.Draw ) -> 2 + 3
+                        | ( Him.Scissor, Result.Loose ) -> 2 + 0
+                        | ( Him.Rock, Result.Loose ) -> 3 + 0
+                        | ( Him.Paper, Result.Win ) -> 3 + 6
+                        | ( Him.Scissor, Result.Draw ) -> 3 + 3
+                        | _ -> -1
+                    )
+                    |> Seq.reduce (+)
+
+
+    printf "part2: %d\n" response
  
 
 [<EntryPoint>]
