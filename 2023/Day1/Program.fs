e051e87d3fd28b1673f12a2fd740587241862212
paraflu <andrea.forlin@gmail.com>
2023-12-01 16:56:48 +0000

    new: dev: day1

--- 2023/Day1/Program.fs
+++ 2023/Day1/Program.fs
@@ -4,7 +4,8 @@ open System
 open NStack
 
 let day1_part1 filename =
-    let rows = readAllTextAsync filename |> Async.RunSynchronously
+    let rows = readAllLines filename |> Async.RunSynchronously
+        |> seq.
     printf "part1: Max calories %d\n" 0
 
 let day1_part2 filename =
@@ -15,6 +16,6 @@ let ustr (x: string) = ustring.Make(x)
 
 [<EntryPoint>]
 let main args =
-    day1_part1 "day1"
-    day1_part2 "day1"
+    day1_part1 "day1.txt"
+    day1_part2 "day1.txt"
     0
