54fa9116decd9e03d9b0948639f3d0a273088378
paraflu <andrea.forlin@gmail.com>
2023-12-03 08:44:16 +0000

    new: dev: regexp fail

--- 2023/Day2/Library.fs
+++ 2023/Day2/Library.fs
@@ -5,12 +5,30 @@ open System.Text.RegularExpressions;
 
 module Run =
 
+    type Color = 
+        | Red of int
+        | Green of int
+        | Blue of int
+
+    let RedCubes = 12
+    let GreenCubes = 13
+    let BlueCubes = 14
+
+    let isPossible game = 0
+
     let parseGame row =
-        let pattern = "Game (?<gameno>\d+):(\s(?<num>\d+) (?<color>red|green|blue)(?<continue>[,;])*"
-        Regex.Matches(row, pattern, RegexOptions.Multiline)
-                                |> Seq.map (fun m -> (m.Groups["color"].Value, m.Groups["num"].Value |> int))
-                                |> Map.ofSeq
-                                |> printf "%A\n" 
+        printf "row %s\n" row
+        // https://regex101.com/r/dUoG1y/1
+        let pattern = @"Game (?<gameno>\d+):(?<set>(\s((?<pick>(?<num>\d+) (?<color>red|green|blue))(?<continue>[,]{0,1}\s*))*);*)*"
+        let matches = Regex.Matches(row, pattern, RegexOptions.Multiline)
+        matches |> Seq.map (fun m ->
+                                    match m.Groups["color"].Value with   
+                                    | "red" -> Red(m.Groups["num"].Value |> int)
+                                    | "green" -> Green(m.Groups["num"].Value |> int)
+                                    | "blue" -> Green(m.Groups["num"].Value |> int)
+                                    | _ -> failwith "Color not found"
+                    )
+        |> printf "ma %A\n" 
 
 
     let part1 filename =
