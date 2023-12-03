37c4dd6c7975c95bc46fa65666ad1019eb66ae20
paraflu <andrea.forlin@gmail.com>
2023-12-03 22:04:24 +0000

    fix: dev: day2

--- 2023/Day2/Library.fs
+++ 2023/Day2/Library.fs
@@ -15,64 +15,90 @@ module Run =
         | Red value -> value
         | Green value -> value
         | Blue value -> value
+
+    let filtraPerTipoColor colore sequenza =
+        sequenza
+        |> Seq.filter (fun c ->
+            match c with
+            | Red _ when colore = "Red" -> true
+            | Green _ when colore = "Green" -> true
+            | Blue _ when colore = "Blue" -> true
+            | _ -> false
+        )
+
+
     let RedCubes = 12
     let GreenCubes = 13
     let BlueCubes = 14
-
-    let isPossible game = 0
 
     let gameImpossible c =  match c with
                                     | Red(v) -> v > RedCubes
                                     | Green(v) -> v > GreenCubes
                                     | Blue(v) -> v > BlueCubes
 
-    let parseGame row =
-        printf "row %s\n" row
-        // https://regex101.com/r/dUoG1y/1
-        // let pattern = @"Game (?<gameno>\d+):(?<set>(\s((?<pick>(?<num>\d+) (?<color>red|green|blue))(?<continue>[,]{0,1}\s*))*);*)*"
-        // let matches = Regex.Matches(row, pattern, RegexOptions.Multiline)
-        // matches[0].Groups
-        //  |> Seq.filter (fun g -> g.Name = "pick")
-        //  |> Seq.map (fun m ->
-        //                             match m["color"].Value with   
-        //                             | "red" -> Red(m["num"].Value |> int)
-        //                             | "green" -> Green(m["num"].Value |> int)
-        //                             | "blue" -> Green(m["num"].Value |> int)
-        //                             | _ -> failwith "Color not found"
-        //             )
-        // |> printf "ma %A\n" 
+    let parseGame row = 
         let matches = Regex.Matches(row, @"Game (?<gameno>\d+):", RegexOptions.Multiline)
         let gameNo = matches[0].Groups[1].Value |> int
-        let colors = row.Split ";" 
-                    |>  Seq.map (fun pick -> 
-                        let m = Regex.Matches(pick, "(?<num>\d+)\s(?<color>red|green|blue)")
-                        m |> Seq.map (fun m -> 
-                                                        printf "%A," m.Groups
-                                                        match m.Groups["color"].Value with   
-                                                        | "red" -> Red(m.Groups["num"].Value |> int)
-                                                        | "green" -> Green(m.Groups["num"].Value |> int)
-                                                        | "blue" -> Green(m.Groups["num"].Value |> int)
-                                                        | _ -> failwith "Color not found"
-                                                        |> gameImpossible
-                        ) |> Seq.exists (fun x -> x)
-                    ) 
 
-        printf "\n%A\n" colors |> Seq.isEmpty
-        match colors |> Seq.isEmpty with
-        | true -> 0
-        | _ -> gameNo
+        let gameSet = row.Split ";" 
+                                    |>  Seq.map (fun pick -> 
+                                        let m = Regex.Matches(pick, "(?<num>\d+)\s(?<color>red|green|blue)")
+
+                                        let colors = m |> Seq.map (fun m -> 
+                                                                        // printf "%A," m.Groups
+                                                                        let color = match m.Groups["color"].Value with   
+                                                                                        | "red" -> Red(m.Groups["num"].Value |> int)
+                                                                                        | "green" -> Green(m.Groups["num"].Value |> int)
+                                                                                        | "blue" -> Blue(m.Groups["num"].Value |> int)
+                                                                                        | _ -> failwith "Color not found"
+                                                                        color )
+                                        colors)
+        (gameNo, gameSet)
+
+    let solutionPart1 row =
+        
+        let (gameNo, gameSet) = parseGame row
+
+        match gameSet |> Seq.concat |> Seq.exists gameImpossible with
+        | false -> gameNo
+        | _ -> 0
+
+    let solutioPart2 row = 
+        let (_, gameSet) = parseGame row
+
+        let maxRed = gameSet 
+                        |> Seq.map (filtraPerTipoColor "Red")
+                        |> Seq.concat
+                        |> Seq.maxBy getColorValue
+        let maxGreen = gameSet 
+                        |> Seq.map (filtraPerTipoColor "Green")
+                        |> Seq.concat
+                        |> Seq.maxBy getColorValue
+
+        let maxBlue = gameSet 
+                        |> Seq.map ( filtraPerTipoColor "Blue")
+                        |> Seq.concat
+                        |> Seq.maxBy getColorValue
+
+        [maxRed; maxGreen; maxBlue] |> Seq.ofList 
+                                    |> Seq.map getColorValue 
+                                    |> Seq.reduce (*)
+
+
 
     let part1 filename =
 
         let row = readAllLinesAsync filename 
                      |> Async.RunSynchronously
         
-        let result= row |> Seq.map parseGame |> Seq.sum
+        let result= row |> Seq.map solutionPart1 |> Seq.sum
 
-        // printf "[!] Part1 %d" result
         result  
 
     let part2 filename =
-        let result = 0
-        // printf "[!] Part2 %d" result
+        let result = readAllLinesAsync filename 
+                            |> Async.RunSynchronously 
+                            |> Seq.map solutioPart2 
+                            |> Seq.sum
+
         result
--- 2023/Day2/Program.fs
+++ 2023/Day2/Program.fs
@@ -5,7 +5,4 @@ let main args =
     part1 "day2.txt" |> printf "[1] result %d\n"
     part2 "day2.txt" |> printf "[2] result %d\n"
 
-    // parserRow "194fivefkdk1" |> printf "%d\n"
-
-
     0
