88ae4ee24d6b6587123bedf19a7a90f6b54c7564
paraflu <andrea.forlin@gmail.com>
2023-12-04 21:28:39 +0000

    day3

--- 2023/Day3/Library.fs
+++ 2023/Day3/Library.fs
@@ -1,49 +1,91 @@
+
 namespace Day3
 open Utility.IO
+open System.Text.RegularExpressions
 
 module Run =
 
-    let trovaAdiacenti (arrayBidimensionale: 'a[,]) (riga: int) (colonna: int) =
-        let maxRighe = Array2D.length1 arrayBidimensionale
-        let maxColonne = Array2D.length2 arrayBidimensionale
 
-        seq {
-            // Controlla l'elemento sopra
-            if riga > 0 then yield arrayBidimensionale.[riga - 1, colonna]
+    let parseNumberFromMatch (m:Match) : int =
+        match m with 
+        | m when m.Length = 0 -> 0
+        | m -> m.Groups[1].Value |> int
 
-            // Controlla l'elemento sotto
-            if riga < maxRighe - 1 then yield arrayBidimensionale.[riga + 1, colonna]
+    let searchInRow (row:string) pos: seq<int> =
 
-            // Controlla l'elemento a sinistra
-            if colonna > 0 then yield arrayBidimensionale.[riga, colonna - 1]
+        match row.[pos] with
+        | x when x = '.' || (System.Char.IsDigit(x) |> not) -> 
+            let before = Regex.Match(row.Substring(0, pos), @"(\d+)$") 
+                        |> parseNumberFromMatch
+                        
+            let after = Regex.Match(row.Substring(pos+1), @"^(\d+)") 
+                        |> parseNumberFromMatch
 
-            // Controlla l'elemento a destra
-            if colonna < maxColonne - 1 then yield arrayBidimensionale.[riga, colonna + 1]
+            seq { before; after }
+        | x when System.Char.IsDigit(x) -> 
+                let middle = Regex.Match(
+                                    row.Substring((max 0 (pos-3))),
+                                    @"[\.](\d+)[\.]")
+                            |> parseNumberFromMatch
+                seq { middle }
+        | x -> failwith $"Carattere {x} non parsato"
+        |> Seq.filter (fun x -> x <> 0)
 
-            // Se desideri controllare anche gli angoli, puoi aggiungere queste condizioni:
-            // Controlla l'elemento in alto a sinistra
-            if riga > 0 && colonna > 0 then yield arrayBidimensionale.[riga - 1, colonna - 1]
+    let getParts pos (rows:seq<string>) =
+        rows 
+        |> Seq.map (fun row -> searchInRow row pos) 
 
-            // Controlla l'elemento in alto a destra
-            if riga > 0 && colonna < maxColonne - 1 then yield arrayBidimensionale.[riga - 1, colonna + 1]
+    let lines (rows:list<string>) idx = 
+        match idx with
+            | 0 -> [ rows.[0] ; rows.[1]]
+            | x when x = (rows |> Seq.length) -> [rows.[x-1];rows.[x]]
+            | x -> [rows.[x-1];rows.[x];rows.[x+1]]
+    let parseGear (pattern:Regex) (rows:list<string>) idx   =
 
-            // Controlla l'elemento in basso a sinistra
-            if riga < maxRighe - 1 && colonna > 0 then yield arrayBidimensionale.[riga + 1, colonna - 1]
-
-            // Controlla l'elemento in basso a destra
-            if riga < maxRighe - 1 && colonna < maxColonne - 1 then yield arrayBidimensionale.[riga + 1, colonna + 1]
-        }
-
+        pattern.Matches(rows.[idx]) 
+            |> Seq.filter (fun m -> m.Length > 0) // seach any match
+            |> Seq.map (fun x -> 
+                lines rows idx 
+                |> getParts x.Index  
+                |> Seq.filter (fun s -> (Seq.length s) > 0)
+                |> Seq.concat 
+            ) 
 
     let part1 filename =
+        let gearRegExp =    Regex(@"[^\d\.]")
 
         let rows = readAllLinesAsync filename 
                      |> Async.RunSynchronously
-        let result = 0
+                     |> List.ofArray
+
+        let result = rows 
+                    |> Seq.mapi (fun idx  _ -> 
+                                                parseGear gearRegExp rows idx
+                                                |> Seq.map (fun s -> Seq.sum s)
+                                )
+                                |> Seq.map Seq.sum
+                    |> Seq.sum
         result  
 
     let part2 filename =
+        let gearRegExp =    Regex(@"\*")
+
         let rows = readAllLinesAsync filename 
-                     |> Async.RunSynchronously
-        let result = 0
+                    |> Async.RunSynchronously
+                    |> List.ofArray
+
+        let result = rows 
+                    |> Seq.mapi (fun idx  _ -> 
+                                                parseGear gearRegExp rows idx
+                                                |> Seq.filter (fun s -> Seq.length s = 2)
+                                                |> Seq.map
+                                                     (fun s ->
+                                                                // let m = Seq.reduce (*) s
+                                                                // printf "part2 %A\n" m
+                                                                // m
+                                                                Seq.reduce (*) s
+                                                    )
+                                )
+                                |> Seq.map Seq.sum
+                    |> Seq.sum
         result  
