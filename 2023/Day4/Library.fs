ed9b5c5e1689af15232dc3f55d99bc28556cdf6c
paraflu <andrea.forlin@gmail.com>
2023-12-04 23:14:07 +0000

    new: dev: day4

--- 2023/Day4/Library.fs
+++ 2023/Day4/Library.fs
@@ -1,19 +1,58 @@
 
 namespace Day4 
 open Utility.IO
+open System.Text.RegularExpressions
 
 module Run =
+
+    let rec score (winsize: list<int>) = 
+          match winsize with
+            | [] -> 0
+            | [_] -> 1
+            | _ :: tail -> ( 2 * score tail)
+
+    let getCards cardList =
+        let matches = Regex.Matches(cardList, @"(\d+)\s*" )
+        matches 
+        |> Seq.cast<Match> 
+        |> Seq.map (fun g -> g.Groups[1].Value |> int)
+        |> Set.ofSeq
+
+    let parseCard row = 
+            let matches = 
+                Regex.Matches(row, @"Card\s+(\d+):\s*(.*)$")
+
+            let m = matches.[0]
+            // let cardNo = m.Groups[1].Value |> int
+            // let cardList = Regex.Matches(m.Groups[2].Value, @"(\d+)[\s\]*"s)
+            let split = Regex.Split(m.Groups[2].Value, @"\|") 
+            let cardList = split|> Array.head |> getCards
+
+            let mineCard = split|> Array.rev |> Array.head |> getCards
+
+
+            let winner = Set.intersect mineCard cardList |> Set.toList
+
+                
+            let s= score winner
+            s
+
 
     let part1 filename =
         let rows = readAllLinesAsync filename 
                      |> Async.RunSynchronously
                      |> List.ofArray
 
-        0
+       
+        let result = rows |> Seq.map parseCard |> Seq.sum   
+
+        result
 
     let part2 filename =
         let rows = readAllLinesAsync filename 
                      |> Async.RunSynchronously
                      |> List.ofArray
+                     
 
-        0
+        let result = 0
+        result
